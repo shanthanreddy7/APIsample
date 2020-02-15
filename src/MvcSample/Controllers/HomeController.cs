192e3e63ffea827809e072db73aee140ff2dff66
Swarupa Rani <Swarupa Rani@DESKTOP-3CQ7448>
2020-02-15 01:58:28 -0500

    Commit

---
+++ src/MvcSample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSample.Controllers.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Home/get-to-do-list")]
+        public List<ToDoList> GetTodoList()
+        {
+            string myJsonString = System.IO.File.ReadAllText(@"./Data/TodoList.json");
+            return JsonConvert.DeserializeObject<List<ToDoList>>(myJsonString);
+        }
+        [HttpPost]
+        [Route("Home/save-to-do-list")]
+        public bool SaveTodoList(ToDoList toDoItem)
+        {
+            string myJsonString = System.IO.File.ReadAllText(@"./Data/TodoList.json");
+            List<ToDoList> existingData = JsonConvert.DeserializeObject<List<ToDoList>>(myJsonString);
+            if (toDoItem.Id > 0)
+            {
+                existingData.Where(x => x.Id == toDoItem.Id).Select(y => y.Status = toDoItem.Status).ToList();
+                System.IO.File.WriteAllText(@"./Data/TodoList.json", JsonConvert.SerializeObject(existingData));
+                return true;
+            }
+            toDoItem.Id = existingData.Count() + 1;
+            existingData.Add(toDoItem);
+            System.IO.File.WriteAllText(@"./Data/TodoList.json", JsonConvert.SerializeObject(existingData));
+            return true;
+
+        }
+    }
+}
---
+++ src/MvcSample/Controllers/Models/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcSample.Controllers.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
